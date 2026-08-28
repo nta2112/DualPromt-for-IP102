a63f6695b2332cd0926497f1a74f0345913ce338
Nguyen Truong An <ann249955@gmail.com>
2026-08-28 09:56:10 +0700

    Fix ImportError: remove resolve_pretrained_cfg from vision_transformer.py

--- vision_transformer.py
+++ vision_transformer.py
@@ -36,7 +36,7 @@ import torch.nn.functional as F
 import torch.utils.checkpoint
 
 from timm.data import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD, IMAGENET_INCEPTION_MEAN, IMAGENET_INCEPTION_STD
-from timm.models.helpers import build_model_with_cfg, resolve_pretrained_cfg, named_apply, adapt_input_conv, checkpoint_seq
+from timm.models.helpers import build_model_with_cfg, named_apply, adapt_input_conv, checkpoint_seq
 from timm.models.layers import PatchEmbed, Mlp, DropPath, trunc_normal_, lecun_normal_
 from timm.models.registry import register_model
 
